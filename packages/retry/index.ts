d88d6f4acc94194e8e1f8cd7cd43aec24a2d4a6a
Ashish K. Poudel <routeasis@gmail.com>
2022-06-27 23:41:56 +0545

    fix npm script

--- packages/retry/index.ts
+++ packages/retry/index.ts
@@ -1,5 +1,5 @@
 export { RetryOptions } from './src/types';
 export { RetryAbortedError } from './src/retry-aborted.error';
 export { RetryFailedError } from './src/retry-failed.error';
-export {} from './src/retry-timeout.error';
+export { RetryTimeoutError } from './src/retry-timeout.error';
 export { retry } from './src/retry';
--- packages/retry/src/retry.ts
+++ packages/retry/src/retry.ts
@@ -3,30 +3,49 @@ import { RetryAbortedError } from './retry-aborted.error';
 import { RetryFailedError } from './retry-failed.error';
 import { RetryTimeoutError } from './retry-timeout.error';
 
-export async function retry<T>(task: () => Promise<T>, options: RetryOptions): Promise<T> {
-  for (let retryCount = 1; retryCount <= options.retries; retryCount++) {
-    try {
-      return await task();
-    } catch (error) {
-      if (error instanceof RetryAbortedError) {
-        throw error;
-      }
-    }
+/**
+ * Decide later: oop or functional
+ */
+
+class Retry {
+  constructor(private readonly options: RetryOptions) {}
+
+  private applyTimeout<T>(fn: () => Promise<T>): Promise<T> {
+    const executionTimeout = this.options?.timeout ?? Infinity;
+
+    return new Promise((resolve, reject) => {
+      const timeoutRef = setTimeout(() => {
+        reject(new RetryTimeoutError('Task retry timeout.'));
+      }, executionTimeout);
+
+      fn()
+        .then((success) => {
+          clearTimeout(timeoutRef);
+          resolve(success);
+        })
+        .catch((error) => reject(error));
+    });
   }
 
-  throw new RetryFailedError('Task retry failed.');
+  async execute<T>(fn: () => Promise<T>): Promise<T> {
+    const task = async () => {
+      for (let retryCount = 1; retryCount <= this.options.retries; retryCount++) {
+        try {
+          return await fn();
+        } catch (error) {
+          if (error instanceof RetryAbortedError) {
+            throw error;
+          }
+        }
+      }
+
+      throw new RetryFailedError('Task retry failed.');
+    };
+
+    return this.applyTimeout(task);
+  }
 }
 
-async function execute<T>(task: () => Promise<T>, timeout?: number): Promise<T> {
-  const executionTimeout = timeout ?? Infinity;
-
-  return new Promise((resolve, reject) => {
-    const timeoutRef = setTimeout(() => {
-      reject(new RetryTimeoutError('Task retry timeout.'));
-    }, executionTimeout);
-
-    task()
-      .then((success) => { clearTimeout(timeoutRef); resolve(success)})
-      .catch((error) => reject(error))
-  });
+export async function retry<T>(fn: () => Promise<T>, options: RetryOptions): Promise<T> {
+  return new Retry(options).execute<T>(fn);
 }
