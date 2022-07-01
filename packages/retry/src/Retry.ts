da7ac63894ef791bbd4d30b0ed3e43c9d6efb82c
Ashish K. Poudel <23501033+ashishkpoudel@users.noreply.github.com>
2022-07-01 23:18:43 +0545

    use functions for retry operations (#7)

--- packages/retry/src/Retry.ts
+++ packages/retry/src/Retry.ts
@@ -1,27 +1,27 @@
 import { RetryOptions } from './types';
-import { Delay } from './operations/Delay';
-import { Timeout } from './operations/Timeout';
+import { delay } from './operations/delay';
+import { applyTimeout } from './operations/applyTimeout';
 import { RetryAbortedError } from './RetryAbortedError';
 import { RetryFailedError } from './RetryFailedError';
 
 export class Retry {
   constructor(private readonly options: RetryOptions) {}
 
-  private get timeout() {
+  private get _timeout() {
     return this.options?.timeout || 0;
   }
 
-  private get delay() {
+  private get _delay() {
     return this.options?.delay || 100;
   }
 
-  private get attempts() {
+  private get _attempts() {
     return this.options.attempts;
   }
 
-  async execute<T>(fn: () => Promise<T>): Promise<T> {
+  async retry<T>(fn: () => Promise<T>): Promise<T> {
     const task = async () => {
-      for (let attempt = 1; attempt <= this.attempts; attempt++) {
+      for (let attempt = 1; attempt <= this._attempts; attempt++) {
         try {
           return await fn();
         } catch (error) {
@@ -29,13 +29,17 @@ export class Retry {
             throw error;
           }
 
-          await new Delay(this.delay, attempt, this.attempts).apply();
+          await delay({
+            delay: this._delay,
+            currentAttempt: attempt,
+            maxAttempts: this._attempts,
+          });
         }
       }
 
       throw new RetryFailedError('Task retry failed.');
     };
 
-    return new Timeout(this.timeout).apply(task);
+    return applyTimeout(task, { timeout: this._timeout });
   }
 }
