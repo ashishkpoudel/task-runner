e60e317f82d39b315520b45196a5912951a54ac1
Ashish K. Poudel <routeasis@gmail.com>
2022-07-04 00:08:35 +0545

    export retry function

--- packages/retry/index.ts
+++ packages/retry/index.ts
@@ -2,4 +2,4 @@ export { RetryOptions } from './src/types';
 export { RetryAbortedError } from './src/RetryAbortedError';
 export { RetryFailedError } from './src/RetryFailedError';
 export { RetryTimeoutError } from './src/RetryTimeoutError';
-export { Retry } from './src/Retry';
+export { retry } from './src/Retry';
